a06af1ef64376762f01bb628e0c5ac3b6c22e24e
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-30 00:41:52 -0300

    :sparkles: feat(Home.jsx): Add basic HTML structure in JS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,10 @@
 import "./styles.css";
 export function Home() {
-  return <h1>Creating app...</h1>;
+  return (
+    <div className="container">
+      <h1>Attendance Confirmation</h1>
+      <input type="text" placeholder="Type name..." />
+      <button type="button">Add</button>
+    </div>
+  );
 }
