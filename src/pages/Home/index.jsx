639eb87dfb68696ce12b154129ff18aef5854597
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-30 01:30:58 -0300

    :sparkles: feat(Home - index): Used the props on the component Card + add function onChange on input to work with the hook 'useState'

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 import { Card } from "../../components/Card/index";
 export function Home() {
+  const [userName, setUserName] = useState();
   return (
     <div className="container">
-      <h1>Attendance Confirmation</h1>
-      <input type="text" placeholder="Type name..." />
+      {/* <h1>Attendance Confirmation</h1> */}
+      <h1>Name: {userName}</h1>
+      <input
+        type="text"
+        placeholder="Type name..."
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
       <button type="button">Add</button>
-      <Card />
-      <Card />
-      <Card />
+      <Card name="Jonas" time="10:14:29" />
+      <Card name="Felipe" time="21:24:27" />
+      <Card name="Nathália" time="08:44:23" />
     </div>
   );
 }
