3ef2d32dc7c7b7e5fd3bf0cdfb42c823b355523d
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-30 01:00:04 -0300

    :sparkles: feat(main.jsx): Imported Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 import "./styles.css";
+
+import { Card } from "../../components/Card/index";
 export function Home() {
   return (
     <div className="container">
       <h1>Attendance Confirmation</h1>
       <input type="text" placeholder="Type name..." />
       <button type="button">Add</button>
+      <Card />
     </div>
   );
 }
