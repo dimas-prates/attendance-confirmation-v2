21268a8095b0f7bd757467d553f77e752ead7908
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-31 20:44:27 -0300

    :sparkles: feat(Home - Page): removed unused import, add type for API, user and corrected the states with them

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 
 import { Card, CardProps } from "../../components/Card/index";
+
+type APIProfileResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name:string;
+  avatar: string;
+}
+
 export function Home() {
-  const [studentName, setStudentName] = useState();
+  const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState<CardProps[]>([]);
-  const [user, setUser] = useState({ name: "", avatar: "" });
+  const [user, setUser] = useState<User>({} as User);
   function handleAddStudent() {
     const newStudent = {
       name: studentName,
@@ -40,7 +51,7 @@ export function Home() {
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("https://api.github.com/users/dimas-prates");
-      const data = await response.json();
+      const data = await response.json() as APIProfileResponse;
       setUser({
         name: data.name,
         avatar: data.avatar_url,
