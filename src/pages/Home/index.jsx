4081e66e4981da3fe4438d08dd8522a72b9a3029
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-31 11:07:33 -0300

    :sparkles: feat(Home.jsx): Made cards dynamic, using useState working with arrays + map and function 'onClick' on the button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,22 +3,40 @@ import "./styles.css";
 
 import { Card } from "../../components/Card/index";
 export function Home() {
-  const [userName, setUserName] = useState();
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    // setStudents([newStudent]);
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
   return (
     <div className="container">
       {/* <h1>Attendance Confirmation</h1> */}
-      <h1>Name: {userName}</h1>
+      <h1>Name: {studentName}</h1>
       <input
         type="text"
         placeholder="Type name..."
         onChange={(e) => {
-          setUserName(e.target.value);
+          setStudentName(e.target.value);
         }}
       />
-      <button type="button">Add</button>
-      <Card name="Jonas" time="10:14:29" />
-      <Card name="Felipe" time="21:24:27" />
-      <Card name="Nathália" time="08:44:23" />
+      <button type="button" onClick={handleAddStudent}>
+        Add
+      </button>
+
+      {students.map((student) => (
+        <Card name={student.name} time={student.time} />
+      ))}
     </div>
   );
 }
